f46f1675d0223511a6489ae0401ee68df0a6d42b
Mareczekk2137 <Markos.Jamrozik@gmail.com>
2023-02-28 19:28:47 +0100

    Initial commit

---
+++ popup.js
@@ -0,0 +1,8 @@
+
+let pickerBtn = document.getElementById("picker")
+
+pickerBtn.addEventListener("click", ()=>{
+    chrome.tabs.query({active: true, currentWindow: true},function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {message: "start"});
+    });
+})
